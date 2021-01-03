ef946252c328bd06fd80ae25fdebcada99ed10ab
Ivan Zapata <falenyo@gmail.com>
2021-01-03 15:07:50 -0500

    html route established

---
+++ routes/html-routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// Router for index.html file
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '../assets/html/index.html'))
+})
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const nodemon = require('nodemon');
+
+app.use(express.static('./assets'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.use(require('./routes/html-routes'))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+});
