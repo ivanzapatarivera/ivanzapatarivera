d668eba330a0812b373a70b323f6fc7bbc91dd5c
Ivan Zapata <falenyo@gmail.com>
2021-01-03 23:59:09 -0500

    about me ok

--- assets/js/eventListeners.js
+++ assets/js/eventListeners.js
@@ -1,7 +1,10 @@
 import { linksBox } from "./eventListener/linksBox.js";
+import { aboutMe } from "./eventListener/aboutMe.js"
 
 function eventListeners() {
     linksBox();
+    aboutMe();
+    contact();
 }
 
 export {eventListeners}
