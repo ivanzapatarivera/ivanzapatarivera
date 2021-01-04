e3170e227600e3388cd4627de25a2f0ab09df80d
Ivan Zapata <falenyo@gmail.com>
2021-01-04 00:08:20 -0500

    event listeners with icons

--- assets/js/eventListeners.js
+++ assets/js/eventListeners.js
@@ -1,11 +1,17 @@
 import { linksBox } from "./eventListeners/linksBox/linksBox.js";
 import { aboutMe } from "./eventListeners/linksBox/aboutMe.js";
 import { contact } from "./eventListeners/linksBox/contact.js";
+import { linkedin } from "./eventListeners/icons/linkedin.js";
+import { github } from "./eventListeners/icons/github.js";
+import { twitter } from "./eventListeners/icons/twitter.js";
 
 function eventListeners() {
   linksBox();
   aboutMe();
   contact();
+  linkedin();
+  github();
+  twitter();
 }
 
 export { eventListeners };
