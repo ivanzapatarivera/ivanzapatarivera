323c692145c8aa90641e1f06be83aa2dd993ce71
Ivan Zapata <falenyo@gmail.com>
2021-01-04 00:18:40 -0500

    formatted JS

--- assets/js/eventListeners/icons/linkedin.js
+++ assets/js/eventListeners/icons/linkedin.js
@@ -1,8 +1,8 @@
 function linkedin() {
-    const linkedin = document.querySelector("#linkedin")
-    linkedin.addEventListener("click", () => {
-        window.open("https://linkedin.com/in/ivanzapatarivera", "_blank")
-    })
+  const linkedin = document.querySelector("#linkedin");
+  linkedin.addEventListener("click", () => {
+    window.open("https://linkedin.com/in/ivanzapatarivera", "_blank");
+  });
 }
 
 export { linkedin };
