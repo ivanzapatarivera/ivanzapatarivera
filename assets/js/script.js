e6573e55347ec72156eed0555a345084cc5b9b58
Ivan Zapata <falenyo@gmail.com>
2021-01-03 19:52:09 -0500

    intro box grid OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,44 @@
-navbar()
-// function createFrame(root, frame) {    
-//     root = document.querySelector('#root');
-
-    // Frame DIV
-    // frame = `<div class="row">
-    //             <div class="col-12" id="frame"></div>
-    //         </div>`;
-    // root.insertAdjacentHTML('afterbegin', frame);
-    // navbar();
-    // content();
-// }
-
+navbar();
 function navbar(frame, navbar, content) {
-    root = document.querySelector('#root');
-    if (root) {
-        navbar =    `<nav class="navbar" id="navbar">
-                        <div class="container">
-                            <div class="navbar-brand">Iv&aacute;n J. Zapata-Rivera</div>
-                            <div class="mr-0">Home</div>
-                        </div>
-                    </nav>`
+  root = document.querySelector("#root");
+  if (root) {
 
-        root.insertAdjacentHTML('afterbegin', navbar)
-        navbar = document.querySelector('#navbar')
-        content =   `<div class="row">
-                        <div class="container col-12 content">This is content
+    // Creating navbar
+    navbar = `<nav class="navbar" id="navbar">
+                <div class="container">
+                    <div class="navbar-brand mx-auto">Iv&aacute;n J. Zapata-Rivera</div>
+                    
+                </div>
+            </nav>`;
+
+    root.insertAdjacentHTML("afterbegin", navbar);
+
+    // Creating main content section
+    navbar = document.querySelector("#navbar");
+    content =   `<div class="row">
+                    <div class="col-12 content" id="content">
+                    </div>
+                </div>`;
+    navbar.insertAdjacentHTML("afterend", content);
+
+    // Creating introduction box grid
+    content = document.querySelector("#content");
+    introBox =  `<div class="row">
+                    <div class="col-12 col-md-6 leftside" id="leftside"></div>
+                    <div class="col-12 col-md-6 rightside" id="rightside">
+                        <div class="row topRow">
                         </div>
-                    </div>`;
-        navbar.insertAdjacentHTML('afterend', content)
-    }
+                        <div class="row middleRow" id="middleRow">
+                            <div class="col-2 leftColumnIntroBox"></div>
+                            <div class="col-8" id="middleColumnIntroBox"></div>
+                            <div class="col-2 rightColumnIntroBox"></div>
+                        </div>
+                        <div class="row bottomRow">
+                        </div>
+                    </div>
+                </div>`
+    content.insertAdjacentHTML("afterbegin", introBox);
+
+    
+  }
 }
