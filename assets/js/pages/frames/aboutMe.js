bfe3e726abda4e843673d880d24a0e767184da70
Ivan Zapata <falenyo@gmail.com>
2021-01-07 22:30:35 -0500

    frame ok

--- assets/js/pages/frames/aboutMe.js
+++ assets/js/pages/frames/aboutMe.js
@@ -7,9 +7,11 @@ function aboutMeFrame() {
 
   // Conditional to verify on aboutMe link
   if (aboutMeLink) {
-    // Creating aboutMePage element/framework
+    // About Me frame
     const aboutMe = `<div class="aboutMePage col-12 col-md-6" style="display: none" id="aboutMePage">
                       </div>`;
+
+    // Inserting frame HTML into About Me DIV
     root.insertAdjacentHTML("afterbegin", aboutMe);
 
     // Calling content to be added into created framework
