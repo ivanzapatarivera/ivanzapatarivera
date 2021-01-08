b01e8ce38d2eb0958deaa461560f8e966cb554c8
Ivan Zapata <falenyo@gmail.com>
2021-01-08 02:23:14 -0500

    added full screen capabilities for mobile screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,33 @@ import { eventListeners } from "./eventListeners.js";
 verifyURL();
 function verifyURL() {
   if (window.origin !== "https://ivanzapatarivera.com") {
-    location.replace("https://ivanzapatarivera.com");
+    document.body.innerHTML = "";
+    loadPage();
+    // location.replace("https://ivanzapatarivera.com");
   } else {
     console.log("You're in ", window.origin);
+    loadPage();
   }
-  loadPage();
+
+  verifyMobileScreen();
+}
+
+function verifyMobileScreen() {
+  const body = document.querySelector("body");
+  window.addEventListener("resize", () => {
+    console.log(document.body.clientWidth)
+    if(document.body.clientWidth <= "767") {
+      body.requestFullscreen();
+      body.addEventListener("fullscreenchange", () => {
+      });
+    } else {
+      document.exitFullscreen()
+    }
+  }, true);
 }
 
 function loadPage() {
-  window.onload = function () {
+  window.onload = () => {
     const root = document.querySelector("#root");
 
     construction(root);
