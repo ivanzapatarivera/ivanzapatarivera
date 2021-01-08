ff1cbe3905edbd161dd8bfc87b6d21a9b7ad4bb5
Ivan Zapata <falenyo@gmail.com>
2021-01-08 01:27:43 -0500

    dynamic generation OK

--- assets/js/pages/content/contact.js
+++ assets/js/pages/content/contact.js
@@ -29,9 +29,9 @@ function contactContents() {
         const type = data.type;
         const content = data.content;
         const href = data.href;
-        const contact = `<div class="row mt-4">
+        const contact = `<div class="row mt-4 link" id="${id}">
                             <div class="col-12 col-md-3"><b>${type}</b></div>
-                            <div class="col-8" id="${id}">${content}</a></div>
+                            <div class="col-8">${content}</a></div>
                         </div>`;
 
         // Adding generated information to contact div element
