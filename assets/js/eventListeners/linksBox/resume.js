dfed3cf3643ad85879f264da757664bee2dcc90a
Ivan Zapata <falenyo@gmail.com>
2021-01-28 10:30:08 -0500

    edited resume link

--- assets/js/eventListeners/linksBox/resume.js
+++ assets/js/eventListeners/linksBox/resume.js
@@ -1,7 +1,7 @@
 function resume() {
     const resume = document.querySelector("#resume");
     resume.addEventListener("click", () => {
-        window.open("../../assets/pdf/IvanZapataRiveraResume-Dec2020.pdf", "_blank")
+        window.open("../../assets/pdf/Ivan J. Zapata Rivera - Resume Update January 2021.pdf", "_blank")
     });
 }
 
