6f07e7c99d27c3a0284e3043133600902dfdcf84
Ivan Zapata <falenyo@gmail.com>
2022-02-05 14:49:17 -0600

    Bugs fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,23 @@ import { construction } from "./construction.js";
 import { eventListeners } from "./eventListeners.js";
 
 // Redirect page to main portfolio URL if coming from other URL's
-// verifyURL();
-// function verifyURL() {
-//   if (window.origin !== "https://ivanzapatarivera.com") {
-//     document.body.innerHTML = ""; 
-//     location.replace("https://ivanzapatarivera.com");
-//   } else {
-//     console.log("You're in ", window.origin);
-//     loadPage();
-//   }
-// }
+let verifyURL = () => {
+  if (window.origin !== "https://ivanzapatarivera.com") {
+    document.body.innerHTML = ""; 
+    location.replace("https://ivanzapatarivera.com");
+  } else {
+    console.log("You're in ", window.origin);
+    loadPage();
+  }
+}
 
 // Loads page constructor and event listeners for page functionality
-function loadPage() {
+let loadPage = () => {
   window.onload = () => {
     const root = document.querySelector("#root");
     construction(root);
     eventListeners();
   };
 }
-loadPage();
+
+verifyURL();
