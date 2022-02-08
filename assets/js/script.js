1eee581bf0191745bfc8697f6febd202266295e6
Ivan Zapata <falenyo@gmail.com>
2022-02-08 12:58:35 -0600

    Enabled verifyURL()

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,15 @@ import { construction } from "./construction.js";
 import { eventListeners } from "./eventListeners.js";
 
 // Redirect page to main portfolio URL if coming from other URL's
-// let verifyURL = () => {
-//   if (window.origin !== "https://ivanzapatarivera.com") {
-//     document.body.innerHTML = ""; 
-//     location.replace("https://ivanzapatarivera.com");
-//   } else {
-//     console.log("You're in ", window.origin);
-//     loadPage();
-//   }
-// }
+let verifyURL = () => {
+  if (window.origin !== "https://ivanzapatarivera.com") {
+    document.body.innerHTML = ""; 
+    location.replace("https://ivanzapatarivera.com");
+  } else {
+    console.log("You're in ", window.origin);
+    loadPage();
+  }
+}
 
 // Loads page constructor and event listeners for page functionality
 let loadPage = () => {
@@ -21,5 +21,4 @@ let loadPage = () => {
   };
 }
 
-// verifyURL();
-loadPage();
+verifyURL();
