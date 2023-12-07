04bf0d3724a4852bd0fc683b5674f2cff29637cd
Ivan Zapata-Rivera <falenyo@gmail.com>
2023-12-06 23:19:46 -0400

    Re-factored code to make it more automated and easier to maintain

--- assets/js/eventListeners/linksBox/resume.js
+++ assets/js/eventListeners/linksBox/resume.js
@@ -1,24 +1,26 @@
-
-var pdf =
-    "../../assets/pdf/Ivan J. Zapata Rivera - Resume Update Aug 2022.pdf",
-  doc =
-    process.ENV.RESUME_DOC,
-  gdoc =
-    "https://docs.google.com/document/d/15GXR3cab9vMoIrFFDqJdDomZKdDz8FgMF8ber6_AWYA";
+var resumeArr = [
+  {
+    selector: "#resume",
+    href: "https://storage.googleapis.com/ivanzapatarivera/resume_pdf.pdf"
+  },
+  {
+    selector: "#resumeDOC",
+    href: "https://storage.googleapis.com/ivanzapatarivera/resume_doc.docx"
+  },
+  {
+    selector: "#resumeGoogleDocs",
+    href: "https://docs.google.com/document/d/15GXR3cab9vMoIrFFDqJdDomZKdDz8FgMF8ber6_AWYA"
+  }
+]
 
 function resume() {
-  const resume = document.querySelector("#resume");
-  resume.addEventListener("click", () => {
-    window.open(pdf, "_blank");
-  });
-  const resumeDOC = document.querySelector("#resumeDOC");
-  resumeDOC.addEventListener("click", () => {
-    window.open(doc, "_blank");
-  });
-  const resumeGoogleDocs = document.querySelector("#resumeGoogleDocs");
-  resumeGoogleDocs.addEventListener("click", () => {
-    window.open(gdoc, "_blank");
-  });
+  for (var resume in resumeArr) {
+    let object = resumeArr[resume];
+    const selector = document.querySelector(object.selector);
+    selector.addEventListener("click", () => {
+      window.open(object.href, "_blank")
+    });
+  }
 }
 
 export { resume };
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@ import { eventListeners } from "./eventListeners.js";
 
 // Redirect page to main portfolio URL if coming from other URL's
 let verifyURL = () => {
-  if (window.origin !== "https://ivanzapatarivera.com") {
-    document.body.innerHTML = ""; 
-    location.replace("https://ivanzapatarivera.com");
-  } else {
+  // if (window.origin !== "https://ivanzapatarivera.com") {
+  //   document.body.innerHTML = ""; 
+  //   location.replace("https://ivanzapatarivera.com");
+  // } else {
     loadPage();
-  }
+  // }
 }
 
 // Loads page constructor and event listeners for page functionality
