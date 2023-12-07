286acd8755acfd6008153328bd54cb7b174b7aad
Ivan Zapata-Rivera <falenyo@gmail.com>
2023-12-06 23:26:14 -0400

    Changed indentations

--- assets/js/pages/frames/getIconsBoxRow.js
+++ assets/js/pages/frames/getIconsBoxRow.js
@@ -1,12 +1,12 @@
 let getIconsBoxRow = () => {
   let iconsBoxRow = document.querySelector("#iconsBoxRow");
   const linksBox = `<div class="row linksBox p-4">
-                            <div class="col-12 text-center boxItems" id="projects">Projects</div>
-                            <div class="col-12 text-center pt-3 boxItems">Resume <span id="resumePDF">PDF</span>, <span id="resumeDOC">Word</span>, <span id="resumeGoogleDocs">Google Docs</span></div>
-                            <div class="col-12 text-center py-3 boxItems" id="aboutMe">About Me</div>
-                            <div class="col-12 text-center boxItems" id="contact">Contact</div>
-                        </div>
-                        `;
+                      <div class="col-12 text-center boxItems" id="projects">Projects</div>
+                      <div class="col-12 text-center pt-3 boxItems">Resume <span id="resumePDF">PDF</span>, <span id="resumeDOC">Word</span>, <span id="resumeGoogleDocs">Google Docs</span></div>
+                      <div class="col-12 text-center py-3 boxItems" id="aboutMe">About Me</div>
+                      <div class="col-12 text-center boxItems" id="contact">Contact</div>
+                    </div>
+                    `;
   iconsBoxRow.insertAdjacentHTML("afterend", linksBox);
   return;
 };
