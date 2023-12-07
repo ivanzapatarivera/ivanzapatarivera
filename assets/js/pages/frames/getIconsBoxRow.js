94d44ea3ed35132269ac59470340acf9500a564a
Ivan Zapata-Rivera <falenyo@gmail.com>
2023-12-06 23:48:09 -0400

    display none for projects

--- assets/js/pages/frames/getIconsBoxRow.js
+++ assets/js/pages/frames/getIconsBoxRow.js
@@ -1,7 +1,7 @@
 let getIconsBoxRow = () => {
   let iconsBoxRow = document.querySelector("#iconsBoxRow");
   const linksBox = `<div class="row linksBox p-4">
-                      <div class="col-12 text-center boxItems" id="projects">Projects</div>
+                      <div class="col-12 text-center boxItems" id="projects" style="display:none">Projects</div>
                       <div class="col-12 text-center pt-3 boxItems">Resume <span id="resumePDF">PDF</span>, <span id="resumeDOC">Word</span>, <span id="resumeGoogleDocs">Google Docs</span></div>
                       <div class="col-12 text-center py-3 boxItems" id="aboutMe">About Me</div>
                       <div class="col-12 text-center boxItems" id="contact">Contact</div>
