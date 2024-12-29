aa219044b85ebf9d0e14e0708134a686db2a7206
Ivan Zapata-Rivera <falenyo@gmail.com>
2024-12-29 01:44:55 -0400

    Moved DF

--- assets/js/pages/frames/getIntroBoxGrid.js
+++ assets/js/pages/frames/getIntroBoxGrid.js
@@ -2,14 +2,7 @@ let getIntroBoxGrid = () => {
     let content = document.querySelector("#content");
     const introBoxGrid = `<div class="row">
                         <div class="col-6 col-md-6 leftside" id="leftside">
-                        </div>
-                        <div class="col-6 col-md-6 rightside" id="rightside">
-                            <div class="row slide-in-right" id="middleRow">                            
-                                <div class="col-11 col-xl-9 mx-auto introBoxColumn" id="introBoxColumn"></div>
-                            </div>
-                            <div class="row slide-in-bottom" id="bottomRow">
-                                <div class="col-11 col-xl-9 mx-auto iconsBoxColum" id="iconsBoxColum"></div>
-                                <!-- Dialogflow Agent Starts Here -->
+                            <!-- Dialogflow Agent Starts Here -->
                                     <link rel="stylesheet" href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css">
                                     <script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
                                     <df-messenger
@@ -49,6 +42,13 @@ let getIntroBoxGrid = () => {
                                         }
                                     </style>
                                     <!-- Dialogflow Agent Ends Here -->
+                        </div>
+                        <div class="col-6 col-md-6 rightside" id="rightside">
+                            <div class="row slide-in-right" id="middleRow">                            
+                                <div class="col-11 col-xl-9 mx-auto introBoxColumn" id="introBoxColumn"></div>
+                            </div>
+                            <div class="row slide-in-bottom" id="bottomRow">
+                                <div class="col-11 col-xl-9 mx-auto iconsBoxColum" id="iconsBoxColum"></div>
                             </div>
                         </div>
                     </div>`;
