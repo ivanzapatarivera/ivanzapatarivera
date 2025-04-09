d02a0f0795e193a3d47b5260b0ed0979e18e0b0d
Ivan Zapata-Rivera <falenyo@gmail.com>
2025-04-09 11:05:15 -0400

    Enabled unauthenticated access

--- assets/js/conv-ai-agent.js
+++ assets/js/conv-ai-agent.js
@@ -5,25 +5,25 @@ var phrase = {
     prompt: "Generate a question offering help or assistance.",
     help: false
 }
-dfMessengerBubble.addEventListener("df-chat-open-changed", async (event) => {
-    const dfMessenger = document.querySelector('df-messenger');
-    if (event.detail.isOpen) {
-        console.log("welcome", welcome)
-        async function startNewSession() {
-            await dfMessenger.startNewSession({ retainHistory: true });
-        }
-        async function sendQuery() {
-            if (welcome) {
-                if (!phrase.help) {
-                    phrase.help = true;
-                    await dfMessenger.sendQuery(phrase.prompt);
-                }
-            } else {
-                await dfMessenger.sendQuery(phrase.welcome);
-                welcome = true;
-            }
-        }
-        startNewSession();
-        sendQuery();
-    }
-});
+// dfMessengerBubble.addEventListener("df-chat-open-changed", async (event) => {
+    // const dfMessenger = document.querySelector('df-messenger');
+    // if (event.detail.isOpen) {
+    //     console.log("welcome", welcome)
+    //     async function startNewSession() {
+    //         await dfMessenger.startNewSession({ retainHistory: true });
+    //     }
+    //     async function sendQuery() {
+    //         if (welcome) {
+    //             if (!phrase.help) {
+    //                 phrase.help = true;
+    //                 await dfMessenger.sendQuery(phrase.prompt);
+    //             }
+    //         } else {
+    //             await dfMessenger.sendQuery(phrase.welcome);
+    //             welcome = true;
+    //         }
+    //     }
+    //     startNewSession();
+    //     sendQuery();
+    // }
+// });
